e45a5858acb7bd8b0a1bbc26e2bc52a84359f558
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-05-24 12:56:17 +0100

    Add Global Styles and Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import Header from './components/Header';
+import { createGlobalStyle } from 'styled-components'
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  a{
+    text-decoration:none;
+    display:block;
+  }
+  `
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <GlobalStyle/>
+        <Header/>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
