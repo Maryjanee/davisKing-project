72648bb79eceb3d657e90315e7929f154f5d25af
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-06-02 11:40:01 +0100

    Add Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { SectionContainer } from '../AboutSection/AboutElements';
+import { NavbarLogo } from '../NavBar/NavbarElements';
+import {FooterItemsContainer,FooterLinksContainer, 
+  FooterLink,FooterNav,FooterNavItem, FooterSocialLinks, 
+  FooterSocialMediaIcon, FooterSocialMediaIconContainer,CopyrightInfoContainer,
+  CopyRightIcon, CopyRightMessage } from '../../components/Footer/FooterElements'
+import facebook from '../../assets/facebook.svg';
+import twitter from '../../assets/twitter.svg';
+import instagram from '../../assets/instagram.svg';
+import youtube from '../../assets/youtube.svg';
+import copyright from '../../assets/copyright.svg';
+
+
+const Footer = () => {
+  return (
+    <>
+    <SectionContainer className="footer-section">
+     <FooterItemsContainer>
+       <NavbarLogo to="/">davisKING</NavbarLogo>
+
+       <FooterLinksContainer>
+         <FooterNav>
+           <FooterNavItem>
+             <FooterLink to="/">
+               Home
+             </FooterLink>
+           </FooterNavItem>
+           <FooterNavItem>
+             <FooterLink to="/about">
+               About
+             </FooterLink>
+           </FooterNavItem>
+
+           <FooterNavItem>
+             <FooterLink to="/discography">
+               Discography
+             </FooterLink>
+           </FooterNavItem>
+           <FooterNavItem>
+             <FooterLink to="/contact">
+               Contact
+             </FooterLink>
+           </FooterNavItem>
+
+         </FooterNav>
+         <FooterSocialLinks>
+                <FooterSocialMediaIconContainer>
+                    <FooterSocialMediaIcon  src={facebook}/>
+                </FooterSocialMediaIconContainer>
+
+              <FooterSocialMediaIconContainer>
+                  <FooterSocialMediaIcon src={instagram}/>
+              </FooterSocialMediaIconContainer>
+
+              <FooterSocialMediaIconContainer>
+                  <FooterSocialMediaIcon src={twitter}/>
+              </FooterSocialMediaIconContainer>
+
+              <FooterSocialMediaIconContainer>
+                  <FooterSocialMediaIcon src={youtube}/>
+              </FooterSocialMediaIconContainer>
+         </FooterSocialLinks>
+      
+       </FooterLinksContainer>
+       <CopyrightInfoContainer>
+         <CopyRightIcon src={copyright} alt="copyright-icon"/>
+         <CopyRightMessage>
+            2021 Davis-king All rights reserved .
+         </CopyRightMessage>
+       </CopyrightInfoContainer>
+     </FooterItemsContainer>
+    </SectionContainer>
+      
+    </>
+  )
+}
+
+export default Footer
