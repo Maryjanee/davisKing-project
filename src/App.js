de188fbcb5c1866e7233280d6b617b0ac96365dd
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-06-28 23:51:55 +0100

    Update App Component with new components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
-import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
-import { createGlobalStyle } from 'styled-components'
-import Home from './pages';
+import React, { useState } from "react";
+import { BrowserRouter, withRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Discography from "./pages/discography";
+import ScrollToTop from "./ScrollToTop";
+import Confirmation from "./components/Confirmation";
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  li{
-    list-style:none;
-  }
-
-  a{
-    text-decoration:none;
-    display:block;
-  }
-  .featuredBtn {
-    margin-left:auto;
-    margin-right:2rem;
-  }
-
-  .footer-section{
-    padding:2rem 0;
-  }
-
-  `
 const App = () => {
   return (
     <BrowserRouter>
-    <GlobalStyle/>
-        <Home/>
+    <ScrollToTop/>
+      <Switch>
+        <Route path="/home" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/discography" component={Discography} />
+        <Route path="/confirmation" component={Confirmation} />
+        <Redirect to="/home" />
+      </Switch>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
