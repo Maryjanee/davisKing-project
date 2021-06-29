941d6984c31115f8ce64f8faf1681bfb9742694d
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-06-29 12:39:18 +0100

    Update About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 import AboutHeader from "../assets/aboutpage.jpg";
-import Navbar from "../components/NavBar";
-import Sidebar from "../components/Sidebar";
-import { ImageBackground } from "../components/Header/HeaderElements";
+import Navbar from "../components/SiteWideComponents/NavBar";
+import Sidebar from "../components/SiteWideComponents/Sidebar";
+import {
+  HeaderTextContainer,
+  ImageBackground,
+} from "../components/SiteWideComponents/Header/HeaderElements";
+import MainAboutSection from "../components/AboutPage";
+import Footer from "../components/SiteWideComponents/Footer";
+import ImageGrid from "../components/ImageGrid";
+import FeaturedVideo from "../components/FeaturedVideo";
+import { Container } from "../components/SiteWideComponents/NavBar/NavbarElements";
+import { MainAboutHeaderContainer,MainAboutHeading, MainAboutQuote, QuoteAuthor } from "../components/SiteWideComponents/AboutSection/AboutElements";
 
 const About = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,10 +26,26 @@ const About = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} bgColor={green} />
-      <ImageBackground
-        bgImg={AboutHeader}
-        bgGradient={aboutPageGradient}
-      ></ImageBackground>
+      <ImageBackground bgImg={AboutHeader} bgGradient={aboutPageGradient}>
+        <Container>
+          <MainAboutHeaderContainer>
+            <MainAboutHeading className="about-title">
+             Favourite Quote
+            </MainAboutHeading>
+            <MainAboutQuote>
+              “Music expresses that which cannot be put into words and that
+              which cannot remain silent.”
+            </MainAboutQuote>
+            <QuoteAuthor>
+              Victor Hugo
+            </QuoteAuthor>
+          </MainAboutHeaderContainer>
+        </Container>
+      </ImageBackground>
+      <MainAboutSection />
+      <ImageGrid />
+      <FeaturedVideo />
+      <Footer />
     </>
   );
 };
